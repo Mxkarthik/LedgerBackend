5e083ca192ee6968cf9265cb306d3b9b47715332
Mxkarthik <maxwhitie1234@gmail.com>
2026-07-03 21:34:20 +0530

    database connected successfully

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const app = require("./src/app")
+require("dotenv").config()
 
+const app = require("./src/app")
+const connectToDB = require("./src/config/db")
+
+
+connectToDB();
 
 app.listen(3000,()=> {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port 3000"); 
 })
